0c09890adce3971b00f684b3641efa9cccb8c4dc
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-07-01 20:29:25 +0500

    add vandetail page
    change

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import  Home from "./Pages/Home.jsx";
-import  About from "./Pages/About.jsx";    
+import  About from "./Pages/About.jsx";
+import  Vans from "./Pages/Vans.jsx";
+import  VanDetail from "./Pages/VanDetail.jsx";
 
 import "./server";
 
@@ -11,15 +13,20 @@ function App() {
   return (
     <BrowserRouter>
     <header>
-      <Link to="/">#VANLIFE My website</Link>
+      <Link to="/">#VANLIFE</Link>
     <nav>
     {/* <Link to="/">Home</Link> */}
     <Link to="/about">About</Link>
+    <Link to="/vans">Vans</Link>
     </nav>
     </header>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/vans" element={<Vans />} />
+      <Route path="/vans/:id/:type" element={<VanDetail />} />
+      {/* <Route path="/vans/:id/:type" element={<VanDetail />} />
+      to show nested route eg */}
     </Routes>
   </BrowserRouter>
   )
