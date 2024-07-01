cbcc682dce03a5a444983f80d74ce6cdcf162199
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-07-01 13:58:21 +0500

    Adding files and gitignore
    Van-life project to learn react-router6

---
+++ src/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import  Home from "./Pages/Home.jsx";
+import  About from "./Pages/About.jsx";           
+
+function App() {
+  return (
+    <BrowserRouter>
+    <header>
+      <Link to="/">#VANLIFE</Link>
+    <nav>
+    {/* <Link to="/">Home</Link> */}
+    <Link to="/about">About</Link>
+    </nav>
+    </header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+// function Home() {
+//   return (
+//     <h1>Hello, React Router its Home 🏠</h1>
+//   )
+// }
+
+// function About() {
+//   return (
+//     <h1>Hello, its About page 🎉</h1>
+//   )
+// }
+
+ReactDOM
+.createRoot(document.getElementById('root'))
+.render(<App />);
