84d758915266e738feeda596edb03306b541ef9e
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-07-02 17:34:22 +0500

    Add Navlinks
    add navlinks in place of links for better user experience to pass props

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,22 @@
-import React from "react"
-import { Outlet } from "react-router-dom"
-import Header from "./Header"
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout() {
     return (
         <>
         {/* <h1>This is the layout route</h1> */}
-            <Header />
+            {/* <Header />
             <Outlet />
+            <Footer />  */}
+                <div className="site-wrapper">
+            <Header />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
         </>
     )
 }
