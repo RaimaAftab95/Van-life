333a20dcd1440c4b0957920a38a933ec02654157
RaimaAftab95 <raima.aftab2022@gmail.com>
2024-07-03 16:00:00 +0500

    Add useOutletcontext hook
    Add useOutletcontext hook to hostvaninfo, hostvanphotos and hostvanpricing pages to pass context as prop in outlet in hostvandetail layout

--- src/Pages/Host/HostVanDetail.jsx
+++ src/Pages/Host/HostVanDetail.jsx
@@ -80,7 +80,7 @@ export default function HostVanDetail() {
                         Photos
                     </NavLink>
                 </nav>
-                <Outlet />
+                <Outlet context={{ currentVan }} />
             </div>
         </section>
     )
